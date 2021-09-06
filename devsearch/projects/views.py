bdacf8d6342b209b188ed9a77583fd8de2264eb3
Julian Grisales <jgrisales@gmail.com>
2021-09-06 14:00:04 +0200

    Templates & Template Inheritance

--- devsearch/projects/views.py
+++ devsearch/projects/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def projects(request):
-    return HttpResponse("Projects here")
+    return render(request, 'projects/projects.html')
 
 def project(request, pk):
-    return HttpResponse(f"Project details code nro: {str(pk)}")
+    return render(request, 'projects/single-project.html', context={"pk": pk})
