2548b580ab5a0c48e04c8dc07e53cdc29dfedf76
Julian Grisales <jgrisales@gmail.com>
2021-09-13 15:37:50 +0200

    Users App

--- devsearch/projects/models.py
+++ devsearch/projects/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 import uuid
+from users.models import Profile
 
 class Project(models.Model):
+    owner = models.ForeignKey(Profile, null=True, blank=True, on_delete=models.SET_NULL) # ManyToOne
     title = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
     featured_image = models.ImageField(null=True, blank=True, default="default.jpg")
@@ -26,7 +28,7 @@ class Review(models.Model):
         ('down', 'Down vote'),
     )
     # owner =
-    project = models.ForeignKey(Project, on_delete=models.CASCADE) # OneToMany
+    project = models.ForeignKey(Project, on_delete=models.CASCADE) # ManyToOne
     body = models.TextField(null=True, blank=True)
     value = models.CharField(max_length=200, choices=VOTE_TYPE)
     created = models.DateTimeField(auto_now_add=True)
