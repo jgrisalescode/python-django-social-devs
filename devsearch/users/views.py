ecccc97514970e46d364058002b61340060d73ad
Julian Grisales <jgrisales@gmail.com>
2021-10-05 16:53:19 +0200

    User Registration and custom form of django

--- devsearch/users/views.py
+++ devsearch/users/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.models import User
 from django.contrib import messages
 from .models import Profile
+from .forms import CustomUserCreationForm
 
 # Create your views here.
 def profiles(request):
@@ -18,6 +19,7 @@ def profile(request, pk):
     return render(request, 'users/profile.html', context=context)
 
 def loginUser(request):
+    page = 'login'
 
     if request.user.is_authenticated:
         return redirect('profiles')
@@ -45,3 +47,26 @@ def logoutUser(request):
     logout(request)
     messages.error(request, 'User was logged out!')
     return redirect('login')
+
+def registerUser(request):
+    page = 'register'
+    form = CustomUserCreationForm()
+
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)  # Presave not in DB yet.
+            user.username = user.username.lower()
+            user.save()  # Saved in DB
+            
+            messages.success(request, 'User account was created')
+
+            login(request, user)
+            return redirect('profiles')
+        else:
+            messages.success(request, 'An error has occurred during registration')
+
+
+
+    context = {'page': page, 'form': form}
+    return render(request, 'users/login_register.html', context=context)
