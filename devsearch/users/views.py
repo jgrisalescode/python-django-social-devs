2e27e73d462de89e9a1555cd7b58de3fb89789e4
Julian Grisales <jgrisales@gmail.com>
2021-10-05 16:07:37 +0200

    User Login, Logout and Flash Messages

--- devsearch/users/views.py
+++ devsearch/users/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.models import User
+from django.contrib import messages
 from .models import Profile
 
 # Create your views here.
@@ -13,3 +16,32 @@ def profile(request, pk):
     otherSkills = profile.skill_set.filter(description="")
     context = {'profile': profile, 'topSkills': topSkills, 'otherSkills': otherSkills}
     return render(request, 'users/profile.html', context=context)
+
+def loginUser(request):
+
+    if request.user.is_authenticated:
+        return redirect('profiles')
+
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'User does not exist')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('profiles')
+        else:
+            messages.error(request, 'Username OR password is not valid')
+
+    return render(request, 'users/login_register.html')
+
+def logoutUser(request):
+    logout(request)
+    messages.error(request, 'User was logged out!')
+    return redirect('login')
