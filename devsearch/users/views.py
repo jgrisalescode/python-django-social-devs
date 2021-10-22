16e87b3a5fb12ebb1133a7643b135009cd252a58
Julian Grisales <jgrisales@gmail.com>
2021-10-22 15:10:34 +0200

    User Edit Profile

--- devsearch/users/views.py
+++ devsearch/users/views.py
@@ -68,7 +68,7 @@ def registerUser(request):
             messages.success(request, 'User account was created')
 
             login(request, user)
-            return redirect('profiles')
+            return redirect('edit-account')
         else:
             messages.success(request, 'An error has occurred during registration')
 
@@ -89,6 +89,15 @@ def userAccount(request):
 
 @login_required(login_url='login')
 def editAccount(request):
-    form = ProfileForm
+    profile = request.user.profile
+    form = ProfileForm(instance=profile)
+
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES, instance=profile)
+        if form.is_valid():
+            form.save()
+
+            return redirect('account')
+
     context = {'form': form}
     return render(request, 'users/profile_form.html', context=context)
